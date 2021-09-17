e89bbbbd1fbe4f8eed96955ac4f774d0d3191598
shiyuan <wangshiyuan@navinfo.com>
2021-09-17 17:00:08 +0800

    配置prettier规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,28 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+    root: true,
+    env: {
+        node: true
+    },
+    extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+    parserOptions: {
+        parser: "babel-eslint"
+    },
+    rules: {
+        "prettier/prettier": [
+            "error",
+            {
+                semi: true,
+                tabWidth: 4,
+                useTabs: false,
+                endOfLine: "lf",
+                arrowParens: "always",
+                trailingComma: "none",
+                jsxSingleQuote: false,
+                jsxBracketSameLine: true,
+                htmlWhitespaceSensitivity: "css"
+            }
+        ],
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    }
 };
